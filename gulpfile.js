527f905bd91f4a9f494564a7f7553599d4f398ca
Patrick Davies <me@patrickdavies.ca>
2018-02-01 23:29:56 -0500

    patch: add package.json and manifest description.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')();
 const del = require('del');
-
+const pkg = require('./package.json');
 
 gulp.task('build', ['copy']);
 
@@ -12,8 +12,14 @@ gulp.task('copy', () => {
 
 gulp.task('package', () => {
   return gulp.src('./built/**/*')
-    .pipe($.zip('./fse.zip'))
+    .pipe($.zip('./fantasy-sports-extension.zip'))
     .pipe(gulp.dest('./output/'));
+});
+
+gulp.task('manifest', () => {
+  return gulp.src('./common/manifest.json')
+    .pipe($.template(pkg))
+    .pipe(gulp.dest('./built'));
 });
 
 gulp.task('watch', () => {
