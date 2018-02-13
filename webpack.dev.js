0257693f9f109f24317408afc3f4838a694ba734
Patrick Davies <me@patrickdavies.ca>
2018-02-13 18:16:22 -0500

    chore: rename output to dist

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,5 +3,10 @@ const common = require('./webpack.common.js');
 const path = require('path');
 
 module.exports = merge(common, {
-  devtool: 'cheap-eval-source-map'
+  devtool: 'cheap-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
 });
