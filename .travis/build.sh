d325b3d58a427389105a0eea12d8981e6cb9a09b
Patrick Davies <me@patrickdavies.ca>
2018-04-07 11:58:22 -0400

    better use of gitversion

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ev;
 yarn test;
-gitversion /output json | json MajorMinorPatch | xargs yarn run build --versionOverride;
+gitversion /output json /showvariable MajorMinorPatch xargs yarn run build --versionOverride;
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
-  gitversion /output json | json MajorMinorPatch | xargs npm version
+  gitversion /output json /showvariable MajorMinorPatch | xargs npm version
   sh .travis/push.sh
 fi
