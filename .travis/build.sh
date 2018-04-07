a895ff7582cbb56cf824b4a7287c63c13a24fbf2
Patrick Davies <me@patrickdavies.ca>
2018-04-07 16:25:06 -0400

    add deploy to chrome web store

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -ev;
 yarn test;
-gitversion /Output json /ShowVariable MajorMinorPatch;
 gitversion /Output json /ShowVariable MajorMinorPatch | xargs yarn run build --versionOverride;
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
