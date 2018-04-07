3a5377611f2f6e0503dfcab52077f2d6e0a02d18
Patrick Davies <me@patrickdavies.ca>
2018-04-07 12:05:21 -0400

    fix typo

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev;
 yarn test;
-gitversion /output json /showvariable MajorMinorPatch xargs yarn run build --versionOverride;
+gitversion /output json /showvariable MajorMinorPatch | xargs yarn run build --versionOverride;
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
   gitversion /output json /showvariable MajorMinorPatch | xargs npm version
