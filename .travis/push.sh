104a68e6bf810dd9f178365120ac4e98b7fdbadb
Patrick Davies <me@patrickdavies.ca>
2018-04-07 16:47:53 -0400

    chore: remove dev/null to see what's going on with the build

--- .travis/push.sh
+++ .travis/push.sh
@@ -2,7 +2,7 @@
 set -ev
 
 push() {
-  git remote add origin https://${GITHUB_OAUTH_TOKEN}@github.com/bleuarg/fantasy-sports-extension.git > /dev/null 2>&1
+  git remote add origin https://${GITHUB_OAUTH_TOKEN}@github.com/bleuarg/fantasy-sports-extension.git # > /dev/null 2>&1
   git push --tags
 }
 
