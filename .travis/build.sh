9afc773fc75da3474ba63ab62ef2a762ee35b589
Patrick Davies <me@patrickdavies.ca>
2018-04-07 17:29:25 -0400

    chore: push tags and code. remove tags condition.

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,8 +5,7 @@ gitversion /Output json /ShowVariable MajorMinorPatch | xargs yarn run build --v
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
   gitversion /Output json /ShowVariable MajorMinorPatch | xargs npm version
-
   git config credential.helper "store --file=.git/credentials"
   echo "https://${GITHUB_OAUTH_TOKEN}:@github.com" > .git/credentials
-  git push --tags
+  git push --follow-tags
 fi
