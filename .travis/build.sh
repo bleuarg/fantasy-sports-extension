49a29807f21ce7932fad12d63245a0442b782981
Patrick Davies <me@patrickdavies.ca>
2018-04-07 12:31:20 -0400

    change case in case it's case sensitive

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ev;
 yarn test;
-gitversion /output json /showvariable MajorMinorPatch | xargs yarn run build --versionOverride;
+gitversion /Output json /ShowVariable MajorMinorPatch | xargs yarn run build --versionOverride;
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
-  gitversion /output json /showvariable MajorMinorPatch | xargs npm version
+  gitversion /Output json /ShowVariable MajorMinorPatch | xargs npm version
   sh .travis/push.sh
 fi
