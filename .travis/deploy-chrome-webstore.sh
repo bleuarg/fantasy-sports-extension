0cfc62a22dd62c1517e7941ebd9667953f7fa82a
Patrick Davies <me@patrickdavies.ca>
2018-04-11 00:30:32 -0400

    fix: add missing params for webstore publish

--- .travis/deploy-chrome-webstore.sh
+++ .travis/deploy-chrome-webstore.sh
@@ -2,4 +2,4 @@
 set -ev;
 
 webstore upload --source dist/chrome.zip --extension-id $GWS_EXTENSION_ID --client-id $GWS_CLIENT_ID --client-secret $GWS_CLIENT_SECRET --refresh-token $GWS_REFRESH_TOKEN
-webstore publish --extension-id $GWS_EXTENSION_ID
+webstore publish --extension-id $GWS_EXTENSION_ID --client-id $GWS_CLIENT_ID --client-secret $GWS_CLIENT_SECRET --refresh-token $GWS_REFRESH_TOKEN
